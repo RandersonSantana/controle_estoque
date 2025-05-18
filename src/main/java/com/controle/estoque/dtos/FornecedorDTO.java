6f0c1e8e2791ec2e752e36c5b0a10bfca47ef698
user <randersondasilvasantana2004@gmail.com>
2025-05-18 18:05:58 -0300

    Criado FornecedorDTO

---
+++ src/main/java/com/controle/estoque/dtos/FornecedorDTO.java
@@ -0,0 +1,53 @@
+package com.controle.estoque.dtos;
+
+public class FornecedorDTO {
+    private Long id;
+
+    private String nome;
+
+    private Double cnpj;
+
+    private String telefone;
+
+    public FornecedorDTO() {
+    }
+
+    public FornecedorDTO(Long id, String nome, Double cnpj, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(Double cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}

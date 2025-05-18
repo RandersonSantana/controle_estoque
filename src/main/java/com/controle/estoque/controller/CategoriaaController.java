0f12b38ad681ab2b8c5b3ab67afba0648e8a8c8e
user <randersondasilvasantana2004@gmail.com>
2025-05-18 18:24:23 -0300

    Criado CategoriaController

---
+++ src/main/java/com/controle/estoque/controller/CategoriaaController.java
@@ -0,0 +1,36 @@
+package com.controle.estoque.controller;
+
+import com.controle.estoque.dtos.CategoriaDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/categorias")
+public class CategoriaaController {
+@Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaDTO> buscarCategoriaPorId(@PathVariable Long id){
+        return ResponseEntity.ok(categoriaService.buscarCategoriaPorId(id));
+    }
+    @GetMapping
+    public ResponseEntity<CategoriaDTO> buscarTodasCategorias(){
+        return ResponseEntity.ok(categoriaService.buscarTodasCategorias());
+    }
+    @PostMapping
+    public ResponseEntity<CategoriaDTO> CadastrarCategoria(@RequestBody CategoriaDTO categoriaDTO){
+        CategoriaDTO categoria = categoriaService.cadastrarCategoria(categoriaDTO);
+        return ResponseEntity.ok(categoria);
+    }
+    @PutMapping
+    public ResponseEntity<CategoriaDTO> atualizarCategoria(@RequestBody CategoriaDTO categoriaDTO){
+        return ResponseEntity.ok(categoriaService.atualizarCategoria(categoriaDTO));
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> deletarCategoria(@RequestBody CategoriaDTO categoriaDTO){
+        categoriaService.deletarCategoria(categoriaDTO.getId());
+        return ResponseEntity.noContent().build();
+    }
+}

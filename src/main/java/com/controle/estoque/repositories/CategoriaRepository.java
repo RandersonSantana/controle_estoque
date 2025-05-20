ac7099a353bb0abf0a408c766e7894a692540ea0
user <randersondasilvasantana2004@gmail.com>
2025-05-20 18:30:23 -0300

    criado CategoriaRepository

---
+++ src/main/java/com/controle/estoque/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.controle.estoque.repositories;
+
+import com.controle.estoque.entities.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
